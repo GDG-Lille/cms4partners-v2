3615eb6cfd56ab851294c10dc30139fbbe7d36ef
Emmanuel DEMEY <demey.emmanuel@gmail.com>
2023-06-11 20:03:36 +0200

    feat: init login

--- apps/front/src/app/app.tsx
+++ apps/front/src/app/app.tsx
@@ -1,11 +1,53 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.scss';
-export function App() {
-  return (
-    <>
-      test
-    </>
-  );
+import * as firebase from 'firebase/app';
+import { getAuth} from 'firebase/auth'
+import { useSignOut, useSignInWithGoogle} from 'react-firebase-hooks/auth';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBzwfrmAx8unwKnGq7DBzu0PD2qlvxcExs",
+  authDomain: "cms4devfest-v2.firebaseapp.com",
+  projectId: "cms4devfest-v2",
+  storageBucket: "cms4devfest-v2.appspot.com",
+  messagingSenderId: "204563655625",
+  appId: "1:204563655625:web:0a23dbae1d0ef27cec9618"
 }
 
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const firebaseAppAuth = getAuth(firebaseApp)
+
+
+
+const App = () => {
+  const [signOut] = useSignOut(firebaseAppAuth);
+  const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(firebaseAppAuth);
+
+  if (loading) {
+    return (
+      <div>
+        <p>Initialising User...</p>
+      </div>
+    );
+  }
+  if (error) {
+    return (
+      <div>
+        <p>Error: {error.message}</p>
+      </div>
+    );
+  }
+
+  if (user) {
+    return (
+      <div>
+        <p>Current User: {user.user.email}</p>
+        <button onClick={signOut}>Log out</button>
+      </div>
+    );
+  }
+  return <button onClick={() => signInWithGoogle()}>Log in</button>;
+  
+}
+
+/** Wrap it */
 export default App;
