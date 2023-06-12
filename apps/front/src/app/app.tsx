a62393ff92d52a6e3a1c828571ae14f9134b1d0e
Emmanuel DEMEY <demey.emmanuel@gmail.com>
2023-06-12 19:14:19 +0200

    feat: add router

--- apps/front/src/app/app.tsx
+++ apps/front/src/app/app.tsx
@@ -1,26 +1,27 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.scss';
 import * as firebase from 'firebase/app';
-import { getAuth} from 'firebase/auth'
-import { useSignOut, useSignInWithGoogle} from 'react-firebase-hooks/auth';
+import { getAuth } from 'firebase/auth';
+import { useSignOut, useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import * as React from "react";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBzwfrmAx8unwKnGq7DBzu0PD2qlvxcExs",
-  authDomain: "cms4devfest-v2.firebaseapp.com",
-  projectId: "cms4devfest-v2",
-  storageBucket: "cms4devfest-v2.appspot.com",
-  messagingSenderId: "204563655625",
-  appId: "1:204563655625:web:0a23dbae1d0ef27cec9618"
-}
+  apiKey: 'AIzaSyBzwfrmAx8unwKnGq7DBzu0PD2qlvxcExs',
+  authDomain: 'cms4devfest-v2.firebaseapp.com',
+  projectId: 'cms4devfest-v2',
+  storageBucket: 'cms4devfest-v2.appspot.com',
+  messagingSenderId: '204563655625',
+  appId: '1:204563655625:web:0a23dbae1d0ef27cec9618',
+};
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
-const firebaseAppAuth = getAuth(firebaseApp)
+const firebaseAppAuth = getAuth(firebaseApp);
 
-
-
-const App = () => {
+const HomePage = () => {
   const [signOut] = useSignOut(firebaseAppAuth);
-  const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(firebaseAppAuth);
+  const [signInWithGoogle, user, loading, error] =
+    useSignInWithGoogle(firebaseAppAuth);
 
   if (loading) {
     return (
@@ -46,8 +47,22 @@ const App = () => {
     );
   }
   return <button onClick={() => signInWithGoogle()}>Log in</button>;
-  
-}
+};
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <HomePage></HomePage>,
+  },
+]);
+
+const App = () => {
+  return (
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>
+  );
+};
 
 /** Wrap it */
 export default App;
